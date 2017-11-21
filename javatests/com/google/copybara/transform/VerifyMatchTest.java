dev-alpha/copybara

commit 2bfb55712192b0e8c6c503d337e0a747bb4eb3fe

Allow transformations to be locally parallel
This allow our transformations to be parallelizable by using a
simple abstraction that allows us to run in parallel a list of
things in batches.

PiperOrigin-RevId: 175248755
Change-Id: I779a3aed2585d4dc9f8a388454290b093ca76eaf

java/com/google/copybara/transform/VerifyMatch.java
@@ -16,11 +16,16 @@
 
 package com.google.copybara.transform;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+
 import com.google.common.base.MoreObjects;
 import com.google.common.base.Preconditions;
+import com.google.common.collect.Iterables;
+import com.google.copybara.LocalParallelizer;
 import com.google.copybara.TransformWork;
 import com.google.copybara.Transformation;
 import com.google.copybara.ValidationException;
+import com.google.copybara.treestate.TreeState.FileState;
 import com.google.copybara.util.Glob;
 import com.google.devtools.build.lib.events.Location;
 import com.google.devtools.build.lib.syntax.EvalException;
@@ -29,6 +34,7 @@ import com.google.re2j.PatternSyntaxException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -40,11 +46,14 @@ public final class VerifyMatch implements Transformation {
   private final Pattern pattern;
   private final boolean verifyNoMatch;
   private final Glob fileMatcherBuilder;
+  private final LocalParallelizer parallelizer;
 
-  private VerifyMatch(Pattern pattern, boolean verifyNoMatch, Glob fileMatcherBuilder) {
+  private VerifyMatch(Pattern pattern, boolean verifyNoMatch, Glob fileMatcherBuilder,
+      LocalParallelizer parallelizer) {
     this.pattern = Preconditions.checkNotNull(pattern);
     this.verifyNoMatch = verifyNoMatch;
     this.fileMatcherBuilder = Preconditions.checkNotNull(fileMatcherBuilder);
+    this.parallelizer = parallelizer;
   }
 
   @Override
@@ -60,18 +69,33 @@ public final class VerifyMatch implements Transformation {
   public void transform(TransformWork work)
       throws IOException, ValidationException {
     Path checkoutDir = work.getCheckoutDir();
-    VerifyMatchVisitor visitor = new VerifyMatchVisitor(pattern,
-        fileMatcherBuilder.relativeTo(checkoutDir), verifyNoMatch);
-    Files.walkFileTree(checkoutDir, visitor);
-    List<String> errors = visitor.getErrors();
+    Iterable<FileState> files = work.getTreeState().find(
+        fileMatcherBuilder.relativeTo(checkoutDir));
+
+    Iterable<String> errors = Iterables.concat(parallelizer.run(files, this::runForFiles));
+
+    int size = 0;
     for (String error : errors) {
+      size++;
       work.getConsole().error(String.format("File '%s' failed validation '%s'.", error,
           describe()));
     }
-    if (errors.size() != 0) {
-      throw new ValidationException(
-          String.format("%d file(s) failed the validation of %s.", errors.size(), describe()));
+    work.getTreeState().notifyNoChange();
+
+    ValidationException.checkCondition(
+        size == 0,
+        "%d file(s) failed the validation of %s.", size, describe());
+  }
+
+  private List<String> runForFiles(Iterable<FileState> files) throws IOException {
+    List<String> errors = new ArrayList<>();
+    for (FileState file : files) {
+      String originalFileContent = new String(Files.readAllBytes(file.getPath()), UTF_8);
+      if (verifyNoMatch == pattern.matcher(originalFileContent).find()) {
+        errors.add(file.toString());
+      }
     }
+    return errors;
   }
 
   @Override
@@ -85,13 +109,13 @@ public final class VerifyMatch implements Transformation {
   }
 
   public static VerifyMatch create(Location location, String regEx, Glob paths,
-      boolean verifyNoMatch) throws EvalException {
+      boolean verifyNoMatch, LocalParallelizer parallelizer) throws EvalException {
     Pattern parsed;
     try {
       parsed = Pattern.compile(regEx, Pattern.MULTILINE);
     } catch (PatternSyntaxException e) {
       throw new EvalException(location, String.format("Regex '%s' is invalid.", regEx), e);
     }
-    return new VerifyMatch(parsed, verifyNoMatch, paths);
+    return new VerifyMatch(parsed, verifyNoMatch, paths, parallelizer);
   }
 }

java/com/google/copybara/BUILD
@@ -85,6 +85,7 @@ BASE_SRCS = [
     "Option.java",
     "Options.java",
     "Origin.java",
+    "LocalParallelizer.java",
     "RepoException.java",
     "Revision.java",
     "Transformation.java",

java/com/google/copybara/Core.java
@@ -660,7 +660,8 @@ public class Core implements OptionsAwareModule, LabelsAwareModule {
       if (mode == Mode.USE_DEFAULT || mode == Mode.SCRUB_NAMES) {
         check(location, mapping.isEmpty(), "'mapping' cannot be used with mode %s", mode);
       }
-      return new TodoReplace(location, paths, tags, mode, mapping, defaultString);
+      return new TodoReplace(location, paths, tags, mode, mapping, defaultString,
+                             self.workflowOptions.parallelizer());
     }
   };
 
@@ -695,9 +696,10 @@ public class Core implements OptionsAwareModule, LabelsAwareModule {
     public VerifyMatch invoke(Core self, String regex, Glob paths, Boolean verifyNoMatch,
         Location location) throws EvalException {
       return VerifyMatch.create(location,
-          regex,
-          paths,
-          verifyNoMatch);
+                                regex,
+                                paths,
+                                verifyNoMatch,
+                                self.workflowOptions.parallelizer());
     }
   };
 

java/com/google/copybara/LocalParallelizer.java
@@ -1,0 +1,86 @@
+/*
+ * Copyright (C) 2017 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.copybara;
+
+import com.google.common.base.Preconditions;
+import com.google.common.base.Throwables;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.ListeningExecutorService;
+import com.google.common.util.concurrent.MoreExecutors;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+
+/**
+ * A class that allows to run a list of things in parallel batches.
+ */
+public class LocalParallelizer {
+
+  private final int threads;
+  private final int minSize;
+  private final ListeningExecutorService executor;
+
+  public LocalParallelizer(int threads, int minSize) {
+    this.threads = threads;
+    this.minSize = minSize;
+    Preconditions.checkState(threads >= 1, "Threads need to be positive");
+    Preconditions.checkState(threads < 1000, "Too many threads (max: 1000)");
+    executor = threads == 1
+        ? null
+        : MoreExecutors.listeningDecorator(Executors.newFixedThreadPool(threads));
+  }
+
+  /**
+   * Run a list of things in batches, calling {@code func} for each batch.
+   */
+  public <K, V> List<V> run(Iterable<K> list, TransformFunc<K, V> func)
+      throws IOException, ValidationException {
+    if (threads == 1 || Iterables.size(list) < minSize) {
+      return ImmutableList.of(func.run(list));
+    }
+    List<ListenableFuture<V>> results = new ArrayList<>(threads);
+    List<K> newList = Lists.newArrayList(list);
+    for (List<K> batch : Lists.partition(newList, Math.max(1, newList.size() / threads))) {
+      results.add(executor.submit(() -> func.run(batch)));
+    }
+    try {
+      return Futures.allAsList(results).get();
+    } catch (InterruptedException e) {
+      Thread.currentThread().interrupt();
+      //TODO We cannot do much here. We might expose InterruptedException all the way up to Main...
+      throw new RuntimeException("Interrupted", e);
+    } catch (ExecutionException e) {
+      Throwables.propagateIfPossible(e.getCause(), IOException.class, ValidationException.class);
+      throw new RuntimeException("Unhandled error", e.getCause());
+
+    }
+  }
+
+  public interface TransformFunc<K, T> {
+
+    /**
+     * Execute oen batch. The number of elements is undefined.
+     */
+    T run(Iterable<K> elements) throws IOException, ValidationException;
+  }
+}

java/com/google/copybara/WorkflowOptions.java
@@ -19,8 +19,10 @@ package com.google.copybara;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.Parameters;
 import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.Suppliers;
 import com.google.copybara.util.console.Console;
 import java.util.Objects;
+import java.util.function.Supplier;
 import javax.annotation.Nullable;
 
 /**
@@ -103,10 +105,25 @@ public class WorkflowOptions implements Option {
           + " branch.")
   public boolean dryRunMode = false;
 
+  @Parameter(names = "--threads",
+      description = "Number of threads to use when running transformations that change lot of files")
+  public int threads = 1;
+
+  @Parameter(names = "--threads-min-size",
+      description = "Minimum size of the lists to process to run them in parallel")
+  public int threadsMinSize = 100;
+
   @Parameter(names = "--notransformation-join",
       description = "By default Copybara tries to join certain transformations in one so that it"
           + " is more efficient. This disables the feature.")
   public boolean noTransformationJoin = false;
+
+  private final Supplier<LocalParallelizer> parallelizerSupplier =
+      Suppliers.memoize(() -> new LocalParallelizer(threads, threadsMinSize));
+
+  public LocalParallelizer parallelizer() {
+    return parallelizerSupplier.get();
+  }
 
   public boolean joinTransformations() {
     return !noTransformationJoin;

java/com/google/copybara/git/GitRepository.java
@@ -378,6 +378,7 @@ public class GitRepository {
     }
     throw new RepoException(String.format("URL '%s' is not valid", url));
   }
+
   /**
    * Execute show-ref git command in the local repository and returns a map from reference name to
    * GitReference(SHA-1).

java/com/google/copybara/transform/Replace.java
@@ -23,6 +23,7 @@ import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Iterables;
+import com.google.copybara.LocalParallelizer;
 import com.google.copybara.NonReversibleValidationException;
 import com.google.copybara.TransformWork;
 import com.google.copybara.Transformation;
@@ -116,20 +117,10 @@ public final class Replace implements Transformation {
     Iterable<FileState> files = work.getTreeState().find(
         fileMatcherBuilder.relativeTo(checkoutDir));
     Replacer replacer = before.replacer(after, firstOnly, multiline, patternsToIgnore);
-    List<FileState> changed = new ArrayList<>();
-    boolean matchedFile = false;
-    for (FileState file : files) {
-      if (Files.isSymbolicLink(file.getPath())) {
-        continue;
-      }
-      matchedFile = true;
-      String originalFileContent = new String(Files.readAllBytes(file.getPath()), UTF_8);
-      String transformed = replacer.replace(originalFileContent);
-      if (!originalFileContent.equals(transformed)) {
-        changed.add(file);
-        Files.write(file.getPath(), transformed.getBytes(UTF_8));
-      }
-    }
+    BatchReplace batchReplace = new BatchReplace(replacer);
+    workflowOptions.parallelizer().run(files, batchReplace);
+    List<FileState> changed = batchReplace.getChanged();
+    boolean matchedFile = batchReplace.isMatchedFile();
     logger.info(String.format("Applied %s to %s files. %s changed.",
                               this,
                               Iterables.size(files),
@@ -205,5 +196,51 @@ public final class Replace implements Transformation {
     return new Replace(
         beforeTokens, afterTokens, parsedGroups, firstOnly, multiline, repeatedGroups, paths,
         parsedIgnorePatterns, workflowOptions);
+  }
+
+  private final static class BatchReplace
+      implements LocalParallelizer.TransformFunc<FileState, Boolean> {
+
+    private final Replacer replacer;
+    private final List<FileState> changed = new ArrayList<>();
+    private boolean matchedFile = false;
+
+    BatchReplace(Replacer replacer) {
+      this.replacer = replacer;
+    }
+
+    public List<FileState> getChanged() {
+      return changed;
+    }
+
+    boolean isMatchedFile() {
+      return matchedFile;
+    }
+
+    @Override
+    public Boolean run(Iterable<FileState> elements) throws IOException, ValidationException {
+      List<FileState> changed = new ArrayList<>();
+      boolean matchedFile = false;
+      for (FileState file : elements) {
+        if (Files.isSymbolicLink(file.getPath())) {
+          continue;
+        }
+        matchedFile = true;
+        String originalFileContent = new String(Files.readAllBytes(file.getPath()), UTF_8);
+        String transformed = replacer.replace(originalFileContent);
+        if (!originalFileContent.equals(transformed)) {
+          synchronized (this) {
+            changed.add(file);
+          }
+          Files.write(file.getPath(), transformed.getBytes(UTF_8));
+        }
+      }
+      synchronized (this) {
+        this.matchedFile |= matchedFile;
+        this.changed.addAll(changed);
+      }
+      // We cannot return null here.
+      return true;
+    }
   }
 }

java/com/google/copybara/transform/TodoReplace.java
@@ -26,6 +26,8 @@ import com.google.common.collect.BiMap;
 import com.google.common.collect.HashBiMap;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Iterables;
+import com.google.copybara.LocalParallelizer;
 import com.google.copybara.NonReversibleValidationException;
 import com.google.copybara.TransformWork;
 import com.google.copybara.Transformation;
@@ -57,6 +59,7 @@ public class TodoReplace implements Transformation {
   private Location location;
   private Glob glob;
   private ImmutableList<String> todoTags;
+  private final LocalParallelizer parallelizer;
   private Mode mode;
   private ImmutableMap<String, String> mapping;
   @Nullable
@@ -64,10 +67,11 @@ public class TodoReplace implements Transformation {
 
   public TodoReplace(Location location, Glob glob, ImmutableList<String> todoTags,
       Mode mode,
-      Map<String, String> mapping, @Nullable String defaultString) {
+      Map<String, String> mapping, @Nullable String defaultString, LocalParallelizer parallelizer) {
     this.location = Preconditions.checkNotNull(location);
     this.glob = Preconditions.checkNotNull(glob);
     this.todoTags = Preconditions.checkNotNull(todoTags);
+    this.parallelizer = parallelizer;
     Preconditions.checkArgument(!todoTags.isEmpty());
     this.mode = Preconditions.checkNotNull(mode);
     this.mapping = Preconditions.checkNotNull(ImmutableMap.copyOf(mapping));
@@ -86,9 +90,13 @@ public class TodoReplace implements Transformation {
 
   @Override
   public void transform(TransformWork work) throws IOException, ValidationException {
-    Path checkoutDir = work.getCheckoutDir();
-    Iterable<FileState> files = work.getTreeState().find(glob.relativeTo(checkoutDir));
+    work.getTreeState().notifyModify(
+        Iterables.concat(
+            parallelizer.run(
+                work.getTreeState().find(glob.relativeTo(work.getCheckoutDir())), this::run)));
+  }
 
+  private Set<FileState> run(Iterable<FileState> files) throws IOException, ValidationException {
     Set<FileState> modifiedFiles = new HashSet<>();
     for (FileState file : files) {
       if (Files.isSymbolicLink(file.getPath())) {
@@ -115,7 +123,7 @@ public class TodoReplace implements Transformation {
         Files.write(file.getPath(), sb.toString().getBytes(UTF_8));
       }
     }
-    work.getTreeState().notifyModify(modifiedFiles);
+    return modifiedFiles;
   }
 
   private List<String> mapUsers(List<String> users, String rawText, Path path)
@@ -172,7 +180,8 @@ public class TodoReplace implements Transformation {
           "Non-reversible mapping: " + e.getMessage());
     }
 
-    return new TodoReplace(location, glob, todoTags, mode, mapping.inverse(), defaultString);
+    return new TodoReplace(location, glob, todoTags, mode, mapping.inverse(), defaultString,
+                           parallelizer);
   }
 
   @Override

commit 6b28ef05170ec408f9333bb1af7ecf0250200ef2

Fix a regression introduced in 2bfb5571219
We didn't print the file name correctly in case of error and instead
we were using the default toString of fileState object.

Tests were wrong because of thrown.expect.

Change-Id: I8acab89777a1c3b469178d49ee30cc6030f87ad0

java/com/google/copybara/transform/VerifyMatch.java
@@ -92,7 +92,7 @@ public final class VerifyMatch implements Transformation {
     for (FileState file : files) {
       String originalFileContent = new String(Files.readAllBytes(file.getPath()), UTF_8);
       if (verifyNoMatch == pattern.matcher(originalFileContent).find()) {
-        errors.add(file.toString());
+        errors.add(file.getPath().toString());
       }
     }
     return errors;

javatests/com/google/copybara/transform/VerifyMatchTest.java
@@ -1,8 +1,11 @@
 package com.google.copybara.transform;
 
+import static com.google.common.truth.Truth.*;
 import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.junit.Assert.fail;
 
 import com.google.common.jimfs.Jimfs;
+import com.google.common.truth.Truth;
 import com.google.copybara.Core;
 import com.google.copybara.ValidationException;
 import com.google.copybara.testing.OptionsBuilder;
@@ -28,9 +31,6 @@ public final class VerifyMatchTest {
   private Path checkoutDir;
   private TestingConsole console;
   private SkylarkTestExecutor skylark;
-
-  @Rule
-  public final ExpectedException thrown = ExpectedException.none();
 
   @Before
   public void setup() throws IOException {
@@ -85,11 +85,14 @@ public final class VerifyMatchTest {
         + ")");
     Path file1 = checkoutDir.resolve("file1.txt");
     writeFile(file1, "bar");
-    thrown.expect(ValidationException.class);
-    thrown.expectMessage("1 file(s) failed the validation of Verify match 'foo'.");
-    transform(transformation);
-    console.assertThat().onceInLog(MessageType.ERROR,
-        "File '/file1.txt' failed validation 'Verify match foo'");
+    try {
+      transform(transformation);
+      fail();
+    } catch (ValidationException e) {
+      assertThat(e.getMessage()).contains("1 file(s) failed the validation of Verify match 'foo'.");
+      console.assertThat().onceInLog(MessageType.ERROR,
+          "File '/file1.txt' failed validation 'Verify match 'foo''.");
+    }
   }
 
   @Test
@@ -100,10 +103,13 @@ public final class VerifyMatchTest {
         + ")");
     Path file1 = checkoutDir.resolve("file1.txt");
     writeFile(file1, "foo");
-    thrown.expect(ValidationException.class);
-    transform(transformation);
-    console.assertThat().onceInLog(MessageType.ERROR,
-        "File '/file1.txt' failed validation 'Verify match foo'");
+    try {
+      transform(transformation);
+      fail();
+    } catch (ValidationException e) {
+      console.assertThat().onceInLog(MessageType.ERROR,
+          "File '/file1.txt' failed validation 'Verify match 'foo''.");
+    }
   }
 
   @Test
@@ -124,12 +130,15 @@ public final class VerifyMatchTest {
         + ")");
 
     prepareGlobTree();
-    thrown.expect(ValidationException.class);
-    transform(transformation);
-    console.assertThat().onceInLog(MessageType.ERROR,
-        "File '/file1.txt' failed validation 'Verify match foo'");
-    console.assertThat().onceInLog(MessageType.ERROR,
-        "File 'folder/file1.txt' failed validation 'Verify match foo'");
+    try {
+      transform(transformation);
+      fail();
+    } catch (ValidationException e) {
+      console.assertThat().onceInLog(MessageType.ERROR,
+          "File '/file1.txt' failed validation 'Verify match 'foo''.");
+      console.assertThat().onceInLog(MessageType.ERROR,
+          "File '/folder/file1.txt' failed validation 'Verify match 'foo''.");
+    }
   }
 
   @Test